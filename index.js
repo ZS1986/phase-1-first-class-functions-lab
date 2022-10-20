9ba80967f5af47037e075d10f19155f8bcc28adc
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-20 11:30:48 -0600

    commit tree 74bdf28da3543fe6f68eea0718f7d2f7a184dece

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice (-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(num){
+    return function (number){
+        return number * num
+    }
+}
+// const fareDoubler = createFareMultiplier(2)
+function fareTripler(num){
+    return num *3
+}
+function fareDoubler(num){
+    return num *2
+}
+function fetchSpecifiedDrivers (array, func){
+    return func (array)
+}
+const selectDifferentDrivers =(arrayOfDrivers, returnFirstTwoDrivers) =>{
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+// function takeTwo(array){
+    array.slice(0,2)
+    //}
+
